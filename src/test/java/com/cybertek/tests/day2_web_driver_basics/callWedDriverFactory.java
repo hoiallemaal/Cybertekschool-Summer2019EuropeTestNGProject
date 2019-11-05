93545f14e7a9d213a45860fa6963071029a1c9c0
Sukru <skrcakmaktepe@gmail.com>
2019-11-05 02:05:45 +0100

    day3 test cases

--- src/test/java/com/cybertek/tests/day2_web_driver_basics/callWedDriverFactory.java
+++ src/test/java/com/cybertek/tests/day2_web_driver_basics/callWedDriverFactory.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 public class callWedDriverFactory {
     public static void main(String[] args) {
 
-        WebDriver driver= WebDriverFactory.getDriver("firefox");
+        WebDriver driver= WebDriverFactory.getDriver("Chrome");
         driver.get("https://www.google.com");
 
     }
